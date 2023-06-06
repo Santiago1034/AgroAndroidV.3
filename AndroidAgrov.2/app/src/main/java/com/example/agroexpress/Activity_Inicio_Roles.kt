ce0b40044db5f34bb3e5b68bdc633fbfbfda3b79
RaulP27 <rperalta0327@gmail.com>
2023-06-06 17:13:18 -0500

    avances 06/06

--- AndroidAgrov.2/app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
+++ AndroidAgrov.2/app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
@@ -16,6 +16,7 @@ class Activity_Inicio_Roles : AppCompatActivity() {
     private val list = mutableListOf<CarouselItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_roles)
         val carousel: ImageCarousel = findViewById(R.id.carousel)
