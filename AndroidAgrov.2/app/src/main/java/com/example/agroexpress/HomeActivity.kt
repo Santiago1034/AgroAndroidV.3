d1f505e3adbe86a1d695be614216a8fdbbdb59c3
Santiago1034 <107003145+Santiago1034@users.noreply.github.com>
2023-06-11 16:30:13 -0500

    bun

--- AndroidAgrov.2/app/src/main/java/com/example/agroexpress/HomeActivity.kt
+++ AndroidAgrov.2/app/src/main/java/com/example/agroexpress/HomeActivity.kt
@@ -3,23 +3,50 @@ package com.example.agroexpress
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Spinner
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.findNavController
 
- class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    private lateinit var myfragment: RegistrarFragment
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        /*val bntRegistar:Button = findViewById(R.id.RegistarUsuario)
-        bntRegistar.setOnClickListener{
-            val intent: Intent = Intent(this, )
+        myfragment = RegistrarFragment()
 
+        val btnRegistar = findViewById<Button>(R.id.RegistarUsuario)
+        btnRegistar.setOnClickListener {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.contenedorRegistarFrament,new RegistrarFragment())
+            fragmentTransaction.replace(R.id.contenedorRegistarFrament,myfragment)
+            fragmentTransaction.commit()
+           // findNavController().navigate(R.id.registrarFragment,myfragment)
+        }
+
+
+
+       /* val bntRegistar:Button = findViewById(R.id.RegistarUsuario)
+        bntRegistar.setOnClickListener{
+            val intent: Intent = Intent(this, cac :: class.java)
+            startActivity(intent)
+        }
+
+        val bntIngresar:Button = findViewById(R.id.IniciarSesionHome)
+        bntIngresar.setOnClickListener{
+            val intent: Intent = Intent(this, cac :: class.java)
+            startActivity(intent)
         }*/
 
 
     }
+
+
 
 
 }
